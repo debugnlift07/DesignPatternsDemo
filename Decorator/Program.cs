e02519f60ab16e135029454d4c44dd6ee3a0ed0e
Naveen Bisht <33859786+debugbyNB@users.noreply.github.com>
2025-09-12 12:22:49 +0530

    Facade desgin patterns

---
+++ Decorator/Program.cs
@@ -0,0 +1,88 @@
+﻿//Purpose: Add behavior to objects dynamically without modifying their code.
+
+//ASP.NET Core Middleware: Each middleware wraps the request/response, adding extra functionality (logging, authentication, exception handling, etc.).
+
+//The request flows through a chain of decorators before reaching the final handler.
+
+
+
+
+//Step 5:client
+class Program
+{
+    static void Main()
+    {
+        IMessageSender sender = new EmailSender();
+
+        // Wrap with decorators
+        sender = new LoggingDecorator(sender);
+        sender = new EncryptionDecorator(sender);
+
+        sender.SendMessage("Hello Naveen!");
+    }
+}
+
+
+
+
+
+//Step1- Component Interface
+
+public interface IMessageSender
+{
+    void SendMessage(string message);
+}
+
+
+//Step2: Concreate Component
+
+public class EmailSender : IMessageSender
+{
+    public void SendMessage(String message)
+    {
+        Console.WriteLine($"Email send: {message}");
+    }
+}
+
+//Step 3: Base Decorator
+
+public abstract class MessageSenderDecorator : IMessageSender
+{
+    protected readonly IMessageSender _sender;
+
+    public MessageSenderDecorator(IMessageSender sender)
+    {
+        _sender = sender;
+    }
+
+    public void SendMessage(string message)
+    {
+        _sender.SendMessage(message); // delegate to wrapped object
+    }
+}
+
+//Step 4: Base Decorator
+public class LoggingDecorator : MessageSenderDecorator
+{
+    public LoggingDecorator(IMessageSender sender) : base(sender) { }
+
+    public override void Send(string message)
+    {
+        Console.WriteLine("[Log] Sending message...");
+        base.SendMessage(message);
+    }
+}
+
+//Step 4: Concrete Decorators
+public class EncryptionDecorator : MessageSenderDecorator
+{
+    public EncryptionDecorator(IMessageSender sender) : base(sender) { }
+
+    public override void Send(string message)
+    {
+        string encrypted = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(message));
+        Console.WriteLine("[Encrypt] Message encrypted.");
+        base.Send(encrypted);
+    }
+}
+
---
+++ Facade/Program.cs
@@ -0,0 +1,81 @@
+﻿
+//Facade Pattern?
+
+//Purpose: Provide a simplified, unified interface to a complex subsystem.
+
+//Instead of the client directly dealing with many complicated classes, the Facade acts as a single entry point.
+
+
+
+//Step 3: Client
+
+  TV tv=new TV();
+  SoundSytem soundSystem = new SoundSytem();
+  Light light1 = new Light();
+
+HomeTheaterFacade home = new HomeTheaterFacade(tv, soundSystem, light1);
+
+Console.WriteLine("****************************************************");
+home.WatchMovie();
+
+Console.WriteLine("\n");
+home.StopWatch();
+
+
+
+
+
+
+
+
+
+//Step 1: Subsystem Classes
+
+
+public class TV
+{
+    public void TurnOn() => Console.WriteLine("TV is on");
+    public void TurnOff() => Console.WriteLine("Off");
+}
+
+public class SoundSytem
+{
+    public void SetSound(string sound) => Console.WriteLine($"Sound is set at {sound}");
+}
+
+public class Light
+{
+    public void DimLights() => Console.WriteLine("Lights are dimmed");
+    public void Brighten() => Console.WriteLine("Lights are brightened");
+}
+
+
+//Step 2: Facade Class
+
+public class HomeTheaterFacade
+{
+    private readonly TV _tv;
+    private readonly SoundSytem _soundSytem;
+    private readonly Light _light;
+    public HomeTheaterFacade(TV tV, SoundSytem soundSytem, Light light)
+    {
+        _tv = tV;
+        _soundSytem = soundSytem;
+        _light = light;
+    }
+
+    public void WatchMovie()
+    {
+        Console.WriteLine("Lets ready for watch movie");
+        _tv.TurnOn();
+        _soundSytem.SetSound("20");
+        _light.DimLights();
+    }
+
+    public void StopWatch()
+    {
+        Console.WriteLine("Shutting down movie mode...");
+        _tv.TurnOff();
+        _light.Brighten();
+    }
+}
